a03a0f447f8e6f0a4f4c7a91adbf3ac3b0a68905
dxc_123 <daixingchuang@163.com>
2022-01-10 14:15:27 +0800

    update

---
+++ SDGiftPlayerManager/SDGiftPlayerManager/cocoapod.h
@@ -0,0 +1,16 @@
+//
+//  cocoapod.h
+//  SDGiftPlayerManager
+//
+//  Created by daixingchuang on 2022/1/10.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface cocoapod : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SDGiftPlayerManager/SDGiftPlayerManager/cocoapod.m
@@ -0,0 +1,12 @@
+//
+//  cocoapod.m
+//  SDGiftPlayerManager
+//
+//  Created by daixingchuang on 2022/1/10.
+//
+
+#import "cocoapod.h"
+
+@implementation cocoapod
+
+@end
