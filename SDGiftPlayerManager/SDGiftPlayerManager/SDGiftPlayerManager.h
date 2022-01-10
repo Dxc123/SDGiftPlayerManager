1430b428e3b1747b6945920ddb9b87e100c5f175
dxc_123 <daixingchuang@163.com>
2022-01-10 19:36:04 +0800

    update

--- SDGiftPlayerManager/SDGiftPlayerManager/SDGiftPlayerManager.h
+++ SDGiftPlayerManager/SDGiftPlayerManager/SDGiftPlayerManager.h
@@ -5,12 +5,12 @@
 //  Created by daixingchuang on 2022/1/10.
 //
 
-#ifndef SDGiftPlayerManager_h
-#define SDGiftPlayerManager_h
-
-//有关VAP礼物特效视频文件播放
-//#import "UIView+VAP.h"
-//#import "QGVAPConfigModel.h"
-//#import "QGVAPWrapView.h"
-
-#endif /* SDGiftPlayerManager_h */
+//#ifndef SDGiftPlayerManager_h
+//#define SDGiftPlayerManager_h
+//
+////有关VAP礼物特效视频文件播放
+////#import "UIView+VAP.h"
+////#import "QGVAPConfigModel.h"
+////#import "QGVAPWrapView.h"
+//
+//#endif /* SDGiftPlayerManager_h */
